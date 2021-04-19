c44397bcde478d1ac0f9f4a0cde922eb7ff48b69
Mehmet Akbaba <mehmet.akbaba2011@gmail.com>
2021-04-20 01:02:35 +0300

    Initial commit

---
+++ Assets/Scripts/RandomColorChange.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomColorChange : MonoBehaviour
+{
+    public float timer = 0.0f;
+    public Material[] material;
+    Renderer rend;
+    public int x;
+    void Start()
+    {
+        x = Random.Range(0,3);
+        rend = GetComponent<Renderer>();
+        rend.enabled = true;
+        rend.material = material[x];
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= 1.0f)
+        {
+            x = Random.Range(0, 3);
+            rend.material = material[x];
+           
+            timer = 0;
+            x = 0;
+        }
+    }
+}
